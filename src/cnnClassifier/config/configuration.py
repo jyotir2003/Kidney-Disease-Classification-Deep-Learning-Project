fe0b2d0279f6e2417a6b3e7559e0d112dbb7e51c
jyotir2003 <jyotiranjan.3.behera@gmail.com>
2024-05-15 07:01:58 +0530

    readme file updated

--- src/cnnClassifier/config/configuration.py
+++ src/cnnClassifier/config/configuration.py
@@ -4,7 +4,8 @@ from cnnClassifier.utils.common import read_yaml, create_directories,save_json
 from cnnClassifier.entity.config_entity import (DataIngestionConfig,
                                                 PrepareBaseModelConfig,
                                                 TrainingConfig,
-                                                )
+                                                EvaluationConfig)
+
 class ConfigurationManager:
     def __init__(
         self,
@@ -73,3 +74,14 @@ class ConfigurationManager:
         )
 
         return training_config
+    
+    def get_evaluation_config(self) -> EvaluationConfig:
+        eval_config = EvaluationConfig(
+            path_of_model="artifacts/training/model.h5",
+            training_data="artifacts/data_ingestion/kidney-ct-scan-image",
+            mlflow_uri="https://dagshub.com/jyotir2003/Kidney-Disease-Classification-Deep-Learning-Project.mlflow",
+            all_params=self.params,
+            params_image_size=self.params.IMAGE_SIZE,
+            params_batch_size=self.params.BATCH_SIZE
+        )
+        return eval_config
